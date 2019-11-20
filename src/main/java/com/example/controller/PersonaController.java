077f72b9eafd1ea8c48f95bdbf4ccb2d33c191d0
sebariquelme <riquelmevillarroel@gmail.com>
2019-11-19 23:24:23 -0300

    verificacion basica en create y update

--- src/main/java/com/example/controller/PersonaController.java
+++ src/main/java/com/example/controller/PersonaController.java
@@ -15,23 +15,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class PersonaController {
-
+	//object to return in errors
+	private Persona personaNoAvailable = new Persona(0,"data error","data error","data error",0,"data error");
+	
 	@RequestMapping(value = "persona", method = RequestMethod.GET)
 	  public List<Persona> list(){
 	    return PersonaData.list();
 	  }
 	  @RequestMapping(value = "persona/create", method = RequestMethod.POST)
 	  public Persona create(@RequestBody Persona persona){
-	    return PersonaData.create(persona);
+		  //Simple verification
+		  if(persona.getName().isEmpty() || persona.getName().equals(null) ) {
+			  return personaNoAvailable;
+		  }else {
+			  return PersonaData.create(persona);
+		  }
 	  }
+	  
 	  @RequestMapping(value = "persona/get/{id}", method = RequestMethod.GET)
 	  public Persona get(@PathVariable Integer id){
 		  return PersonaData.get(id);
 	  }
+	  
 	  @RequestMapping(value = "persona/update/{id}", method = RequestMethod.PUT)
 	  public Persona update(@PathVariable Integer id, @RequestBody Persona persona){
-	    return PersonaData.update(id, persona);
+		  if(persona.getName().isEmpty() || persona.getName().equals(null) ) {
+			  return personaNoAvailable;
+		  }else {
+			  return PersonaData.update(id, persona);
+		  }
 	  }
+	  
+
 	  @RequestMapping(value = "persona/delete/{id}", method = RequestMethod.DELETE)
 	  public Persona delete(@PathVariable Integer id){
 	    return PersonaData.delete(id);
