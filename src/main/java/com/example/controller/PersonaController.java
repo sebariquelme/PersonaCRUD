829526d3672ffcb09593321775a6d8f185d987f4
sebariquelme <riquelmevillarroel@gmail.com>
2019-11-19 23:46:53 -0300

    final commit

--- src/main/java/com/example/controller/PersonaController.java
+++ src/main/java/com/example/controller/PersonaController.java
@@ -1,4 +1,5 @@
 package com.example.controller;
+
 import com.example.data.PersonaData;
 import com.example.models.*;
 
@@ -15,41 +16,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class PersonaController {
-	//object to return in errors
-	private Persona personaNoAvailable = new Persona(0,"data error","data error","data error",0,"data error");
-	
-	@RequestMapping(value = "persona", method = RequestMethod.GET)
-	  public List<Persona> list(){
-	    return PersonaData.list();
-	  }
-	  @RequestMapping(value = "persona/create", method = RequestMethod.POST)
-	  public Persona create(@RequestBody Persona persona){
-		  //Simple verification
-		  if(persona.getName().isEmpty() || persona.getName().equals(null) ) {
-			  return personaNoAvailable;
-		  }else {
-			  return PersonaData.create(persona);
-		  }
-	  }
-	  
-	  @RequestMapping(value = "persona/get/{id}", method = RequestMethod.GET)
-	  public Persona get(@PathVariable Integer id){
-		  return PersonaData.get(id);
-	  }
-	  
-	  @RequestMapping(value = "persona/update/{id}", method = RequestMethod.PUT)
-	  public Persona update(@PathVariable Integer id, @RequestBody Persona persona){
-		  if(persona.getName().isEmpty() || persona.getName().equals(null) ) {
-			  return personaNoAvailable;
-		  }else {
-			  return PersonaData.update(id, persona);
-		  }
-	  }
-	  
+	// object to return in errors
+	private Persona personaNoAvailable = new Persona(0, "data error", "data error", "data error", 0, "data error");
 
-	  @RequestMapping(value = "persona/delete/{id}", method = RequestMethod.DELETE)
-	  public Persona delete(@PathVariable Integer id){
-	    return PersonaData.delete(id);
-	  }
+	@RequestMapping(value = "persona", method = RequestMethod.GET)
+	public List<Persona> list() {
+		return PersonaData.list();
+	}
+
+	@RequestMapping(value = "persona/create", method = RequestMethod.POST)
+	public Persona create(@RequestBody Persona persona) {
+		// Simple validation to name empty
+		if (persona.getName().isEmpty() || persona.getName().equals(null)) {
+			return personaNoAvailable;
+		} else {
+			return PersonaData.create(persona);
+		}
+	}
+
+	@RequestMapping(value = "persona/get/{id}", method = RequestMethod.GET)
+	public Persona get(@PathVariable Integer id) {
+		return PersonaData.get(id);
+	}
+
+	@RequestMapping(value = "persona/update/{id}", method = RequestMethod.PUT)
+	public Persona update(@PathVariable Integer id, @RequestBody Persona persona) {
+		// Simple validation to name empty
+		if (persona.getName().isEmpty() || persona.getName().equals(null)) {
+			return personaNoAvailable;
+		} else {
+			return PersonaData.update(id, persona);
+		}
+	}
+
+	@RequestMapping(value = "persona/delete/{id}", method = RequestMethod.DELETE)
+	public Persona delete(@PathVariable Integer id) {
+		return PersonaData.delete(id);
+	}
 
 }
